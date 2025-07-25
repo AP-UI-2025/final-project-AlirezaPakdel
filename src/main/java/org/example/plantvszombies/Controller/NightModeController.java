0689c5e0ef65b2f81d503f1799fdedf1269946eb
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-07-25 07:21:11 +0330

    show missions

--- src/main/java/org/example/plantvszombies/Controller/NightModeController.java
+++ src/main/java/org/example/plantvszombies/Controller/NightModeController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -23,6 +24,21 @@ public class NightModeController {
     private ImageView BackBtn;
 
     @FXML
+    private Label mission1;
+
+    @FXML
+    private Label mission2On;
+
+    @FXML
+    private Label mission2off;
+
+    @FXML
+    private Label mission3Off;
+
+    @FXML
+    private Label mission3On;
+
+    @FXML
     void GoBack(MouseEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("HomePageView.fxml"));
         Scene scene = new Scene(loader.load());
@@ -33,8 +49,29 @@ public class NightModeController {
 
     @FXML
     void initialize() {
+        if (HelloController.logInPlayer.getLevel()>=5){
+            mission2off.setVisible(false);
+            mission3Off.setVisible(false);
+            mission3Off.setVisible(false);
+            mission2off.setVisible(false);
+        }
+        if (HelloController.logInPlayer.getLevel()==4){
+            mission3On.setVisible(false);
+            mission3On.setVisible(false);
+            mission2off.setVisible(false);
+            mission2off.setVisible(false);
+        }
+
         assert BackBtn != null : "fx:id=\"BackBtn\" was not injected: check your FXML file 'NightModeView.fxml'.";
 
     }
 
+    public void mission1(MouseEvent mouseEvent) {
+    }
+
+    public void mission2(MouseEvent mouseEvent) {
+    }
+
+    public void mission3(MouseEvent mouseEvent) {
+    }
 }
