e5b9d7915e20eefa6c879f332b8ab2ffbdcd957c
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-08-01 10:34:08 +0330

    game root is for all missions!

--- src/main/java/org/example/plantvszombies/Controller/LevelOneController.java
+++ src/main/java/org/example/plantvszombies/Controller/LevelOneController.java
@@ -7,11 +7,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import org.example.plantvszombies.Model.Game.GameRoot;
 import org.example.plantvszombies.Model.Game.GameState;
 import org.example.plantvszombies.Model.Plants.PeaShooter;
+import org.example.plantvszombies.Model.Plants.SunFlower;
 import org.example.plantvszombies.Model.Zombies.NormalZombie;
 
 import java.net.URL;
@@ -25,22 +30,96 @@ public class LevelOneController implements Initializable {
 
     @FXML
     private ImageView background;
+    private enum PlantType { PEASHOOTER, SUNFLOWER , NONE}
+    private PlantType selectedPlant = PlantType.NONE;
 
-    private final int CELL_WIDTH = 85;
-    private final int CELL_HEIGHT = 100;
-    private final int GRID_ROWS = 5;
-    private final int GRID_COLS = 9;
-    private int sun = 1000;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         background.setImage(new Image(getClass().getResource("/images/DayBackground.png").toExternalForm()));
         GameRoot.setInstance();
         GameState.NewGameState();
+        GameRoot.getInstance().setGamePane(gamePane);
+
+
+        HBox plantBar = new HBox(20);
+        plantBar.setLayoutX(250);
+        plantBar.setLayoutY(30);
+        plantBar.setPrefHeight(30);
+        plantBar.setStyle("-fx-background-color: #dfd360;");
+
+
+        Image peashooterImage = new Image(getClass().getResource("/images/PeashooterSeedPacket.png").toExternalForm());
+        ImageView peashooterIcon = new ImageView(peashooterImage);
+        peashooterIcon.setFitWidth(50);
+        peashooterIcon.setFitHeight(70);
+        peashooterIcon.setOnMouseClicked(e -> {
+            selectedPlant = PlantType.PEASHOOTER;
+            System.out.println("Peashooter selected");
+        });
+
+        Image sunflowerImage = new Image(getClass().getResource("/images/SunflowerSeedPacket.png").toExternalForm());
+        ImageView sunflowerIcon = new ImageView(sunflowerImage);
+        sunflowerIcon.setFitWidth(50);
+        sunflowerIcon.setFitHeight(70);
+        sunflowerIcon.setOnMouseClicked(e -> {
+            selectedPlant = PlantType.SUNFLOWER;
+            System.out.println("Sunflower selected");
+        });
+
+        plantBar.getChildren().addAll(peashooterIcon, sunflowerIcon);
+        gamePane.getChildren().add(plantBar);
+
+
+        GridPane grid = new GridPane();
+        grid.setLayoutX(220);
+        grid.setLayoutY(220);
+        grid.setStyle("-fx-border-color: #81782a;");
+        gamePane.getChildren().add(grid);
+        for (int row = 0; row < GameRoot.getInstance().getROWS(); row++) {
+            for (int col = 0; col < GameRoot.getInstance().getCOLS(); col++) {
+                Rectangle tile = new Rectangle(GameRoot.getInstance().getTILE_WIDTH(), GameRoot.getInstance().getTILE_HEIGHT());
+                tile.setFill(Color.rgb(0, 0, 0, 0));
+                grid.add(tile, col, row);
+                tile.setStroke(Color.DARKGREEN);
+                //tile.setStyle("-fx-border-color: #81782a;");
+                //tile.setStyle("-fx-border-color: #00ffcc;");
+                int finalRow = row;
+                int finalCol = col;
+
+                tile.setOnMouseClicked(e -> {
+                    if (selectedPlant == PlantType.NONE) {
+                        System.out.println("No plant selected.");
+                        return;
+                    }
+
+                    switch (selectedPlant) {
+                        case PEASHOOTER:
+                            if (GameState.getInstance().getSunPoints()>=100) {
+                                GameState.getInstance().setSunPoints(GameState.getInstance().getSunPoints()-100);
+                                PeaShooter peaShooter = new PeaShooter(grid.getLayoutY() + finalRow * GameRoot.getInstance().getTILE_HEIGHT() , grid.getLayoutX() + finalCol * GameRoot.getInstance().getTILE_WIDTH()+20);
+                                break;
+                            }else{
+                                break;
+                            }
+                        case SUNFLOWER:
+                            if (GameState.getInstance().getSunPoints()>=50) {
+                                GameState.getInstance().setSunPoints(GameState.getInstance().getSunPoints()-50);
+                                SunFlower sunFlower = new SunFlower(grid.getLayoutY() + finalRow * GameRoot.getInstance().getTILE_HEIGHT() , grid.getLayoutX() + finalCol * GameRoot.getInstance().getTILE_WIDTH()+20);
+                                break;
+                            }else{
+                                break;
+                            }
+                    }
+
+                    selectedPlant = PlantType.NONE;
+                });
+            }
+        }
+
 
 
         spawnZombies();
-        GameRoot.getInstance().setGamePane(gamePane);
     }
 
 
--- src/main/java/org/example/plantvszombies/Model/Game/GameRoot.java
+++ src/main/java/org/example/plantvszombies/Model/Game/GameRoot.java
@@ -16,12 +16,27 @@ import org.example.plantvszombies.Model.Plants.PeaShooter;
 import org.example.plantvszombies.Model.Plants.SunFlower;
 
 public class GameRoot {
+    public int getROWS() {
+        return ROWS;
+    }
+
+    public int getCOLS() {
+        return COLS;
+    }
+
+    public int getTILE_WIDTH() {
+        return TILE_WIDTH;
+    }
+
+    public int getTILE_HEIGHT() {
+        return TILE_HEIGHT;
+    }
+
     private final int ROWS = 5;
     private final int COLS = 9;
     private final int TILE_WIDTH = 80;
     private final int TILE_HEIGHT = 100;
-    private enum PlantType { PEASHOOTER, SUNFLOWER , NONE}
-    private PlantType selectedPlant = PlantType.NONE;
+
     Label sunLabel;
 
 
@@ -72,82 +87,6 @@ public class GameRoot {
         gamePane.getChildren().add(sunNum);
 
 
-        HBox plantBar = new HBox(20);
-        plantBar.setLayoutX(250);
-        plantBar.setLayoutY(30);
-        plantBar.setPrefHeight(30);
-        plantBar.setStyle("-fx-background-color: #dfd360;");
-
-
-        Image peashooterImage = new Image(getClass().getResource("/images/PeashooterSeedPacket.png").toExternalForm());
-        ImageView peashooterIcon = new ImageView(peashooterImage);
-        peashooterIcon.setFitWidth(50);
-        peashooterIcon.setFitHeight(70);
-        peashooterIcon.setOnMouseClicked(e -> {
-            selectedPlant = PlantType.PEASHOOTER;
-            System.out.println("Peashooter selected");
-        });
-
-        Image sunflowerImage = new Image(getClass().getResource("/images/SunflowerSeedPacket.png").toExternalForm());
-        ImageView sunflowerIcon = new ImageView(sunflowerImage);
-        sunflowerIcon.setFitWidth(50);
-        sunflowerIcon.setFitHeight(70);
-        sunflowerIcon.setOnMouseClicked(e -> {
-            selectedPlant = PlantType.SUNFLOWER;
-            System.out.println("Sunflower selected");
-        });
-
-        plantBar.getChildren().addAll(peashooterIcon, sunflowerIcon);
-        gamePane.getChildren().add(plantBar);
-
-
-        GridPane grid = new GridPane();
-        grid.setLayoutX(220);
-        grid.setLayoutY(220);
-        grid.setStyle("-fx-border-color: #81782a;");
-        gamePane.getChildren().add(grid);
-        for (int row = 0; row < ROWS; row++) {
-            for (int col = 0; col < COLS; col++) {
-                Rectangle tile = new Rectangle(TILE_WIDTH, TILE_HEIGHT);
-                tile.setFill(Color.rgb(0, 0, 0, 0));
-                grid.add(tile, col, row);
-                tile.setStroke(Color.DARKGREEN);
-                //tile.setStyle("-fx-border-color: #81782a;");
-                //tile.setStyle("-fx-border-color: #00ffcc;");
-                int finalRow = row;
-                int finalCol = col;
-
-                tile.setOnMouseClicked(e -> {
-                    if (selectedPlant == PlantType.NONE) {
-                        System.out.println("No plant selected.");
-                        return;
-                    }
-
-                    switch (selectedPlant) {
-                        case PEASHOOTER:
-                            if (GameState.getInstance().getSunPoints()>=100) {
-                                GameState.getInstance().setSunPoints(GameState.getInstance().getSunPoints()-100);
-                                loadSunNum();
-                                PeaShooter peaShooter = new PeaShooter(grid.getLayoutY() + finalRow * TILE_HEIGHT , grid.getLayoutX() + finalCol * TILE_WIDTH+20);
-                                break;
-                            }else{
-                                break;
-                            }
-                        case SUNFLOWER:
-                            if (GameState.getInstance().getSunPoints()>=50) {
-                                GameState.getInstance().setSunPoints(GameState.getInstance().getSunPoints()-50);
-                                loadSunNum();
-                                SunFlower sunFlower = new SunFlower(grid.getLayoutY() + finalRow * TILE_HEIGHT , grid.getLayoutX() + finalCol * TILE_WIDTH+20);
-                                break;
-                            }else{
-                                break;
-                            }
-                    }
-
-                    selectedPlant = PlantType.NONE;
-                });
-            }
-        }
     }
 
     private void loadSunNum() {
