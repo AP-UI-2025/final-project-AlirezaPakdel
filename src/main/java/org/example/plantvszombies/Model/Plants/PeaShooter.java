30664f239013d89bebb484b89b5f15290077e74d
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-08-01 12:50:22 +0330

    fix pea shooter

--- src/main/java/org/example/plantvszombies/Model/Plants/PeaShooter.java
+++ src/main/java/org/example/plantvszombies/Model/Plants/PeaShooter.java
@@ -35,7 +35,8 @@ public class PeaShooter extends WarriorPlant {
         Timeline shooter = new Timeline(new KeyFrame(Duration.seconds(1.5), event -> {
             boolean zombieInRow = false;
             for (ImageView zombie : GameState.getInstance().getZombies()) {
-                if (Math.abs(zombie.getLayoutY() + 15 - getRow()) < 15) {
+                System.out.println("zombie y : " + zombie.getLayoutY() + " plant y : " + getRow());
+                if (Math.abs(zombie.getLayoutY()  - getRow()) <= 30) {
                     zombieInRow = true;
                     break;
                 }
