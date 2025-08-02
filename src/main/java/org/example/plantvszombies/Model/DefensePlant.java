e83178d42d9ce47b311b2799fe6147420cc810a4
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-08-02 19:07:24 +0330

    wall nut is here

--- src/main/java/org/example/plantvszombies/Model/DefensePlant.java
+++ src/main/java/org/example/plantvszombies/Model/DefensePlant.java
@@ -1,15 +1,44 @@
 package org.example.plantvszombies.Model;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
+import org.example.plantvszombies.Model.Game.GameState;
+
+import java.beans.EventHandler;
+
 public class DefensePlant extends Plant {
     private int health;
+    private Image scondImage;
 
-
-    public DefensePlant(int solarCost, String plantName , int health , int x , int y) {
-        super(solarCost, plantName , x, y);
-        this.health = health;
+    public Image getScondImage() {
+        return scondImage;
     }
 
-    public static void BlockZombie(){}
+    public void setScondImage(Image scondImage) {
+        this.scondImage = scondImage;
+    }
+
+    public DefensePlant(int solarCost, String plantName , int health , int x , int y ) {
+        super(solarCost, plantName , x, y);
+        this.health = health;
+        BlockZombie();
+    }
+
+    public void BlockZombie(){
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), e->{
+            if (this.scondImage != null) {
+                if (super.getImageView().getImage() != this.scondImage) {
+                    if (GameState.getInstance().getPlantsHealth().get(super.getImageView().getImage()) <= 25) {
+                        super.getImageView().setImage(scondImage);
+                    }
+                }
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
 
 
     public int getHealth() {
---
+++ src/main/java/org/example/plantvszombies/Model/Plants/WallNut.java
@@ -0,0 +1,28 @@
+package org.example.plantvszombies.Model.Plants;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.example.plantvszombies.Model.DefensePlant;
+import org.example.plantvszombies.Model.Game.GameRoot;
+import org.example.plantvszombies.Model.Game.GameState;
+
+public class WallNut extends DefensePlant {
+    public WallNut(double row, double col , int x, int y) {
+        super(50, "WallNut", 30, x, y);
+        Image image2 = new Image(getClass().getResource("/images/wallNutSecond.gif").toExternalForm());
+        super.setScondImage(image2);
+        Image image = new Image(getClass().getResource("/images/wallnut.gif").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(70);
+        imageView.setFitWidth(60);
+        imageView.setLayoutX(col);
+        imageView.setLayoutY(row);
+        GameRoot.getInstance().getGamePane().getChildren().add(imageView);
+        imageView.setPreserveRatio(true);
+        super.setImageView(imageView);
+        GameState.getInstance().getPlants().add(imageView);
+        GameState.getInstance().getPlantsHealth().put(imageView, 5);
+        setCol(col);
+        setRow(row);
+    }
+}
