ed3e2453a133c4704ed5386d087e719b884769b8
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-08-02 12:21:20 +0330

    Repeater is in

---
+++ src/main/java/org/example/plantvszombies/Model/Plants/Repeater.java
@@ -0,0 +1,64 @@
+package org.example.plantvszombies.Model.Plants;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import org.example.plantvszombies.Model.BulletType;
+import org.example.plantvszombies.Model.Game.GameRoot;
+import org.example.plantvszombies.Model.Game.GameState;
+import org.example.plantvszombies.Model.Game.TimelineManager;
+import org.example.plantvszombies.Model.WarriorPlant;
+
+public class Repeater extends WarriorPlant {
+    public Repeater(double row, double col , int x, int y) {
+        super(200, "Repeater", 10, 2, BulletType.Normal, x, y);
+        Image image = new Image(getClass().getResource("/images/repeater.gif").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(70);
+        imageView.setFitWidth(60);
+        imageView.setPreserveRatio(true);
+        imageView.setLayoutX(col);
+        imageView.setLayoutY(row);
+        super.setImageView(imageView);
+        GameState.getInstance().getPlants().add(imageView);
+        GameState.getInstance().getPlantsHealth().put(imageView, 5);
+        setRow(row);
+        setCol(col);
+        GameRoot.getInstance().getGamePane().getChildren().add(imageView);
+        StartPeaShooter();
+    }
+
+
+    private void StartPeaShooter() {
+        Timeline shooter = new Timeline(new KeyFrame(Duration.seconds(1.5), event -> {
+            boolean zombieInRow = false;
+            for (ImageView zombie : GameState.getInstance().getZombies()) {
+                if (Math.abs(zombie.getLayoutY()  - getRow()) <= 30) {
+                    zombieInRow = true;
+                    break;
+                }
+            }
+
+            if (zombieInRow) {
+                if (super.getShootTimeline() == null) {
+                    Timeline sh = new Timeline(new KeyFrame(Duration.seconds(0.5) , er ->{
+                        startShooting();
+                    }));
+                    sh.setCycleCount(2);
+                    sh.play();
+                }
+            } else {
+                if (super.getShootTimeline() != null) {
+                    super.getShootTimeline().stop();
+                    super.setShootTimeline(null);
+                }
+            }
+        }));
+        shooter.setCycleCount(Timeline.INDEFINITE);
+        shooter.play();
+        TimelineManager.getInstance().add(shooter);
+        super.getAllTimelines().add(shooter);
+    }
+}
