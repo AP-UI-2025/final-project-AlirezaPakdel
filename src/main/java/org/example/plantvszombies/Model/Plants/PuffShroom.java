f1c58f7d22b1f290893d7ea6d94138235cb428de
Alireza Pakdel <alirezapakdel8405@gmail.com>
2025-08-03 10:32:30 +0330

    puff shroom available

---
+++ src/main/java/org/example/plantvszombies/Model/Plants/PuffShroom.java
@@ -0,0 +1,61 @@
+package org.example.plantvszombies.Model.Plants;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import org.example.plantvszombies.Model.BulletType;
+import org.example.plantvszombies.Model.Game.GameRoot;
+import org.example.plantvszombies.Model.Game.GameState;
+import org.example.plantvszombies.Model.Game.TimelineManager;
+import org.example.plantvszombies.Model.WarriorPlant;
+
+public class PuffShroom extends WarriorPlant {
+    public PuffShroom(double row, double col , int x , int y) {
+        super(0, "puffShroom", 5, 4, BulletType.Smoke, x, y);
+
+        Image image = new Image(getClass().getResource("/images/puff_ShroomRelax.gif").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(70);
+        imageView.setFitWidth(60);
+        imageView.setPreserveRatio(true);
+        imageView.setLayoutX(col);
+        imageView.setLayoutY(row);
+        super.setImageView(imageView);
+        GameState.getInstance().getPlants().add(imageView);
+        GameState.getInstance().getPlantsHealth().put(imageView, 5);
+        setRow(row);
+        setCol(col);
+        GameRoot.getInstance().getGamePane().getChildren().add(imageView);
+        StartPeaShooter();
+    }
+
+
+    private void StartPeaShooter() {
+        Timeline shooter = new Timeline(new KeyFrame(Duration.seconds(1.5), event -> {
+            boolean zombieInRow = false;
+            for (ImageView zombie : GameState.getInstance().getZombies()) {
+                if (Math.abs(zombie.getLayoutY()  - getRow()) <= 30) {
+                    zombieInRow = true;
+                    break;
+                }
+            }
+
+            if (zombieInRow) {
+                if (super.getShootTimeline() == null) {
+                    startShooting();
+                }
+            } else {
+                if (super.getShootTimeline() != null) {
+                    super.getShootTimeline().stop();
+                    super.setShootTimeline(null);
+                }
+            }
+        }));
+        shooter.setCycleCount(Timeline.INDEFINITE);
+        shooter.play();
+        TimelineManager.getInstance().add(shooter);
+        super.getAllTimelines().add(shooter);
+    }
+}
